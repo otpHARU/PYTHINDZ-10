050bf8f91a10d76305bc98f03890444b49240c94
Haru <harumolodec@mail.ru>
2022-11-16 04:21:40 +0700

    compl END

---
+++ compl.py
@@ -0,0 +1,11 @@
+def compl_1(items):
+    x1 = float(items[0])
+    x2 = float(items[1])
+    x = complex(x1, x2)
+    return x
+
+def compl_2(items):
+    y1 = float(items[2])
+    y2 = float(items[3])
+    y = complex(y1, y2)
+    return y
